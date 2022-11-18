b45d4ecc34732b1218c02b3e3adc804537d7416f
Tyler French <tylerfrench89@gmail.com>
2022-11-18 15:49:46 -0800

    fleshed out nav module and added css colors and reset

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('test again')
+import './style.css'
+const content = document.getElementById('content')
+
+const nav = () => {
+  const links = ['home', 'menu', 'contact']
+
+  const nav = document.createElement('nav')
+  nav.classList.add('nav')
+  
+  const brand = document.createElement('a')
+  brand.classList.add('brand-logo')
+  brand.innerText = 'your brand'
+
+  const signIn = document.createElement('a')
+  signIn.classList.add('sign-in')
+  signIn.innerText = 'SIGN IN'
+
+  const ul = document.createElement('ul')
+  for (let i = 0; i < links.length; i++) {
+    const li = document.createElement('li')
+    li.innerText = links[i]
+    li.classList.add('nav-link')
+    ul.appendChild(li)
+  }
+
+  nav.appendChild(brand)
+  nav.appendChild(ul)
+  nav.appendChild(signIn)
+
+  return nav
+}
+
+content.appendChild(nav())
