4c5366631ece4f2cd585394a8ce59c0bafa4a0ba
Tyler French <tylerfrench89@gmail.com>
2022-12-01 15:17:44 -0800

    ability to see new page when clicking link

--- src/index.js
+++ src/index.js
@@ -10,14 +10,31 @@ const content = document.getElementById('content')
 export default function changePage(that) {
   let selectedPage = that.target.dataset.page
   let currentPage = content.firstChild.dataset.page
+  let page = content.firstChild
+  console.log(page)
   if (currentPage === selectedPage) return
-  clearPage(selectedPage, currentPage)
+  clearPage(page)
+  appendPage(selectedPage)
 }
 
-function clearPage(sPage, cPage) {
-  console.log(content.firstChild, 'content data set')
-  let page = content.firstChild
+function clearPage(page) {
   content.removeChild(page)
+}
+
+function appendPage(page) {
+  console.log(page)
+  switch (page) {
+    case 'home':
+      content.appendChild(home())
+      break
+    case 'menu':
+      content.appendChild(menu())
+      break
+    case 'contact':
+      content.appendChild(contact())
+      break
+  }
+  console.log('test')
 }
 
 header.appendChild(nav())
