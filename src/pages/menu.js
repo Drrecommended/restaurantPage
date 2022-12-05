247edd78ecc2046e9960791ca253d218faf070cf
Tyler French <tylerfrench89@gmail.com>
2022-12-05 15:24:28 -0800

    added JSON and imported it into the module

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,8 +1,31 @@
+import menuData from '../menu.json'
+
 const menu = () => {
-    const hello = document.createElement('div')
-    hello.dataset.page = 'menu'
-    hello.textContent = "WELCOME menu"
-    return hello
+    const menu = document.createElement('div')
+    menu.dataset.page = 'menu'
+    menu.classList.add('menu-container')
+    const menuTitle = document.createElement('h2')
+    menuTitle.innerText = "MENU"
+    menu.appendChild(menuTitle)
+    menuData.pizzas.forEach(pizza => {
+        const itemContainer = document.createElement('div')
+        itemContainer.classList.add('item-grid')
+        const item = document.createElement('h4')
+        item.innerText = pizza.type
+        const price = document.createElement('p')
+        price.innerText = `$${pizza.cost}`
+        const description = document.createElement('p')
+        description.classList.add('description')
+        const pic = document.createElement('div')
+        pic.classList.add('menu-pic')
+        description.innerText = pizza.description
+        itemContainer.appendChild(item)
+        itemContainer.appendChild(price)
+        itemContainer.appendChild(description)
+        menu.appendChild(itemContainer)
+    })
+    
+    return menu
 }
 
 
